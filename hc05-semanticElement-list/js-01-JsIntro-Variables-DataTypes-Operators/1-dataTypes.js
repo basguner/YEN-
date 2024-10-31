2c916afbef5d51408db855a9f562800142c3fdb8
bahar <basgunerbahar@gmail.com>
2024-10-31 21:24:22 +0300

    select

---
+++ hc05-semanticElement-list/js-01-JsIntro-Variables-DataTypes-Operators/1-dataTypes.js
@@ -0,0 +1,7 @@
+//********DATA TYPES****** /
+//number :tam sayıları ve ondalıklı sayıları tutar
+
+
+
+const age=true
+console.log(typeof age);
---
+++ hc05-semanticElement-list/js-01-JsIntro-Variables-DataTypes-Operators/js-02-Data/js-03-Operators_Conditionals/Operators.js
@@ -0,0 +1,58 @@
+/****OPERATORS******/
+//!Aritmetik operatorler
+
+let a=10
+let b=2
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a ** 100);
+console.log(a % b);//mod alma operatorü :kalan bulma
+
+
+//öncelik sıralaması-işlem önceliği
+//üs parantez içi,çarpma/bölme,çıkarma toplama
+//saat ve dakika 
+
+//dakika olarak verilen aşağdaki değer kaç saat kaç dk ya denk gelmektdir
+
+let süre=377
+let saat=Math.floor(sure/60);
+console.log(saat);
+let dakika=sure%60;
+console.log(dakika);
+console.log(`${saat}:${dakika}`);
+
+//ıncrement-decrement//
+let x=10
+//x++önce yazdır sonra artır
+//++ x önce artır sonra yazdır
+console.log("Increment");
+console.log(++x);
+console.log("decrement");
+
+
+
+//x + önce yazdır sonra azalt
+// ++ x önce alazalt sonra yzdır
+
+//x=x-1
+console.log(x--);
+console.log(--x);
+
+
+//ASSIGMENTLER
+LET T=7
+LET Z=6
+console.log(+);
+//ATAMA OPERATORU=
+t=z // z değerini t ye aktarıyoruz
+console.log(t);
+
+
+
+t=t+Z
+console.log(t);
+t +=5
+console.log(object);
