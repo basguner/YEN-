76f59727fcd58c1ef023a673863be40cdaaba3b9
bahar <basgunerbahar@gmail.com>
2024-11-06 21:26:59 +0300

    nnn

---
+++ hc05-semanticElement-list/bahar/script.js
@@ -0,0 +1,4 @@
+function reverseWord(sentence){
+    let result="";
+    
+}
---
+++ hc05-semanticElement-list/js-01-JsIntro-Variables-DataTypes-Operators/js-02-Data/js-03-Operators_Conditionals/js-04-Loops/js-f5-Function/2-functionExpression.js
@@ -0,0 +1,21 @@
+// ? =========================================================
+// ?                    FONKSİYONLAR
+// ?==========================================================
+
+//!----------------------------------------------------------
+//! 2.YÖNTEM  : FUNCTION EXPRESSION
+//!-----------------------------------------------------------
+
+//**********************************************************/
+//! * --------Function-Expression----------------------------*/
+// 1-Fonksiyon bir değişkene tanımlanır.
+// 2-Function kelimesini ardından fonksiyonun ismi verilir.
+// 3-Sonra da bir parantez açılıp kapatılır.
+//? önce fonksiyon oluşturmak şart - hoisting
+
+
+
+const adYazdir=function(){
+    console.log("merhaba");
+}
+adYazdir()
