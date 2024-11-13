8b2f82c048d4b9396cc7a674e49d5dd7f08cba42
bahar <basgunerbahar@gmail.com>
2024-11-13 22:56:18 +0300

    hhh

---
+++ CLASS-NOTES/code-class-01/apps.js
@@ -0,0 +1,27 @@
+//condiditional//
+//loop//
+//function//
+//string//
+
+//1-condiditionl
+//karemi dikdörtgenmi? (conditional ve function)
+//iki kenar uzunluğu verilen fonsiyona parametre olarak gönderelim bir şeklin karemi dikdörtgenmi olduğunu belirliyen bir fonksiyon yaz
+
+
+let kenar1 =50
+let kenar2 =40
+
+function check(a,b){
+// if (a==b){
+//     console.log("kare");
+// }
+// else{
+//     console.log("dikdörtgen");
+// }
+
+a==b ? console.log("kare") : console.log("dikdörtgen");
+
+
+}
+
+check(kenar1,kenar2)
