2ca1e5a475b5301782c647e974fe966ac934fba8
bahar <basgunerbahar@gmail.com>
2024-11-18 20:18:20 +0300

    hhh

---
+++ js-06-strings/js-07-Arrays/1-arrays.js
@@ -0,0 +1,19 @@
+// ?==============================================
+// ?            DIZILER (ARRAYS)
+// ?==============================================
+//?JavaScript'te array (dizi), birden fazla değeri tek bir değişken içinde depolamamızı sağlayan bir veri yapısıdır.
+//? Diziler, farklı türde verileri (sayılar, stringler, booleanlar vb.) aynı anda tutabilir ve indeks numaralarıyla erişilebilirler. 
+console.log("***********arrays******");
+//*DİZİ TANIMLAMA***/
+//*---------------------
+//!1-SGUARE BRACKET (aaray litersl)yöndemi ile dize tanımlama
+//!en çok tercih edilen yöntem
+
+
+/*boş array */
+const numbers=[]
+//!,arrayler için de birçok tür değişken olabilir
+const names=["ahmet" "berk" "34,78, true ,null ,"undefined" [3,6,9]]
+
+    console.log(names);
+    console.log(names[3]);
